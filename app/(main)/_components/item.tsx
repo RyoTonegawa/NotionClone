52e79f95def4cfdd63e919bf30fee6a655286a2c
RyoTonegawa <tonegawa622@gmail.com>
2024-06-15 23:36:18 +0900

    fix

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -64,6 +64,7 @@ export const Item = ({
       error: "Failed to archive note.",
     });
   };
+
   const handleExpand = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
@@ -126,7 +127,7 @@ export const Item = ({
       {!!id && (
         <div className="ml-auto flex items-center gap-x-2">
           <DropdownMenu>
-            <DropdownMenuTrigger onClick={(e) => e.stopPropagation()} asChild>
+            <DropdownMenuTrigger asChild>
               <div
                 role="button"
                 className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
