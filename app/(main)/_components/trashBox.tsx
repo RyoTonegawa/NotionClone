50ec526b488cf576302e953635a6e9012c914699
RyoTonegawa <tonegawa622@gmail.com>
2024-06-19 00:15:20 +0900

    Trash and restore , delete confirm dialog

--- app/(main)/_components/trashBox.tsx
+++ app/(main)/_components/trashBox.tsx
@@ -5,6 +5,9 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useState } from "react";
 import { toast } from "sonner";
+import { Spinner } from "@/components/spinner";
+import { Search, Trash, Undo } from "lucide-react";
+import { Input } from "@/components/ui/input";
 
 const TrashBox = () => {
   const router = useRouter();
@@ -37,7 +40,73 @@ const TrashBox = () => {
     });
   };
 
-  return <div>Trash Box</div>;
+  const onRemove = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    documentId: Id<"documents">
+  ) => {
+    const promise = remove({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Removing note...",
+      success: "Note removed!",
+      error: "Failed to remove note",
+    });
+
+    if (params.documentId === documentId) {
+      router.push("/documents");
+    }
+    if (documents === undefined) {
+      return (
+        <div className="h-full flex itens-center justify-center p-4">
+          <Spinner size="lg" />
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div className="text-sm">
+      <div className="flex items-center justify-center gap-x-1 p-2">
+        <Search className="h-4 w-4" />
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+          placeholder="filter by page title..."
+        />
+      </div>
+      <div className="mt-2 px-1 pb-1">
+        <p className="hidden last:block text-xs  text-center text-muted-foreground pb-2">
+          No documents found
+        </p>
+        {filteredDocuments?.map((document) => (
+          <div
+            key={document._id}
+            role="button"
+            onClick={() => onClick(document._id)}
+            className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center justify-between text-primary"
+          >
+            <span className="truncate pl-2">{document.title}</span>
+            <div className="flex items-center">
+              <div
+                onClick={(e) => onRestore(e, document._id)}
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200"
+              >
+                <Undo className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <div
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200"
+              >
+                <Trash className="h-4 w-4 text-muted-foreground" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TrashBox;
---
+++ components/modal/comfirm-modal.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+interface ConfirmModalProps {
+  children: React.ReactNode;
+  onConfirm: () => void;
+}
+
+const ComfirmModal = ({ children, onConfirm }: ConfirmModalProps) => {
+  return <div></div>;
+};
+
+export default ComfirmModal;
